efb36f46604c46d3cc9135d1147f0a384aed8a9e
Valentin <valentinjadot@gmail.com>
2022-09-26 14:32:11 -0300

    feat(API) : add both url for localhost and prod url

--- src/utils/hitEndpoint.js
+++ src/utils/hitEndpoint.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
-const baseUrl = process.env.REACT_APP_API_BASE_URL;
+const baseUrl = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return process.env.REACT_APP_API_BASE_URL;
+  }
+  return '';
+};
 
 export default async function hitEndpoint(query, payload, type = 'get') {
   try {
-    const url = baseUrl + query;
+    const url = baseUrl() + query;
     console.log(url);
     const { data } = await axios[type](url, payload);
     return data;
